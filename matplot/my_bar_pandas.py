23f2425ea8e147ad03e9c238b321511fea5b69f7
guoliang wang <tswanggl@hotmail.com>
2021-02-08 20:51:52 +0800

    two csv read method 1. csv module 			2. pandas pd.read_csv 			3. collections Counter() method useage

--- matplot/my_bar_pandas.py
+++ matplot/my_bar_pandas.py
@@ -2,30 +2,39 @@ import csv
 import numpy as np
 from matplotlib import pyplot as plt
 from collections import Counter
+import pandas as pd
 
 plt.style.use('fivethirtyeight')
 
-with open('../data/data.csv', 'r') as csv_file:
-    csv_reader = csv.DictReader(csv_file)
+def read_plot_with_pandas(filename):
+    data = pd.read_csv(filename)
+
+    ids = data['Responder_id']
+    lang_responses = data['LanguagesWorkedWith']
+
     language_counter = Counter()
 
-    for row in csv_reader:
-        language_counter.update(row['LanguagesWorkedWith'].split(';'))
+    for response in lang_responses:
+        language_counter.update(response.split(';'))
 
-languages = []
-popularity = []
+    languages = []
+    popularity = []
 
-for item in language_counter.most_common(15):
-    languages.append(item[0])
-    popularity.append(item[1])
+    for item in language_counter.most_common(15):
+        languages.append(item[0])
+        popularity.append(item[1])
 
-languages.reverse()
-popularity.reverse()
+    languages.reverse()
+    popularity.reverse()
 
-plt.barh(languages, popularity)
-plt.title("Most Popular Languages")
-# plt.ylabel('Programming Languages')
-plt.xlabel('Number Of People Who Use')
+    plt.barh(languages, popularity)
+    plt.title("Most Popular Languages")
+    # plt.ylabel('Programming Languages')
+    plt.xlabel('Number Of People Who Use')
 
-plt.tight_layout()
-plt.show()
+    plt.tight_layout()
+    plt.show()
+
+
+if __name__ == "__main__":
+    read_plot_with_pandas('../data/data.csv')
