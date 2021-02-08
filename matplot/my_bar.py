b08e1a944cf9ba17684758c35d5a71254be46a40
guoliang wang <tswanggl@hotmail.com>
2021-02-08 17:48:31 +0800

    Add data set for bar

--- matplot/my_bar.py
+++ matplot/my_bar.py
@@ -1,21 +1,30 @@
+import numpy as np
 import matplotlib.pyplot as plt
 
 
 def barPrc():
-    x = [1, 2, 3]
-    y = [2, 2, 3]
+    plt.style.use('fivethirtyeight')
+    ages_x = [25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35]
 
-    x1 = [1, 4, 6]
-    y1 = [1, 1, 5]
+    x_indexes = np.arange(len(ages_x))
+    width = 0.25
 
-    plt.bar(x, y, label="bar 1", color="r")
-    plt.bar(x1, y1, label="bar 2", color="g")
+    dev_y = [38496, 42000, 46752, 46732, 53200, 56000, 62316, 64928, 67137, 68748, 73752]
+    py_dev_y = [45372, 48876, 53850, 57287, 63016, 65998, 70003, 70000, 71496, 75370, 83640]
+    js_dev_y = [37810, 43515, 46823, 49293, 53437, 56373, 62375, 66674, 68745, 68746, 74583]
 
-    plt.plot(x, y, label="First line")
-    plt.plot(x1, y1, label="second line")
-    plt.xlabel("Plot Number")
-    plt.ylabel("Important var")
+    plt.bar(x_indexes - width, dev_y, width=width, color="#78FDFF", label="bar 1")
+    plt.bar(x_indexes, py_dev_y, width=width, color="#FF3D4A", label="bar 2")
+    plt.bar(x_indexes + width, js_dev_y, width=width, color="#223D4A", label="bar 2")
+
+    plt.xticks(ticks=x_indexes, labels=ages_x)
+    plt.xlabel("Ages")
+    plt.ylabel("Median Salary (USD) by Age")
     plt.title("Interesting Graph\nCheck it out")
     plt.legend()
 
     plt.show()
+
+
+if __name__ == "__main__":
+    barPrc()
--- matplot/my_plot.py
+++ matplot/my_plot.py
@@ -2,18 +2,27 @@ import matplotlib.pyplot as plt
 
 
 def plotLine():
-    x = [1, 2, 3]
-    y = [2, 2, 3]
+    ages_x = [25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35]
+    dev_y = [38496, 42000, 46752, 46732, 53200, 56000, 62316, 64928, 67137, 68748, 73752]
+    py_dev_y = [45372, 48876, 53850, 57287, 63016, 65998, 70003, 70000, 71496, 75370, 83640]
+    js_dev_y = [37810, 43515, 46823, 49293, 53437, 56373, 62375, 66674, 68745, 68746, 74583]
 
-    x1 = [1, 4, 6]
-    y1 = [1, 1, 5]
-
-    plt.plot(x, y, label="First line")
-    plt.plot(x1, y1, label="second line")
+    plt.plot(ages_x, dev_y, color='#FF707A', linestyle='--', marker='.', label='All Devs')
+    plt.plot(ages_x, py_dev_y, color='#5a7d9a', linestyle='-.', marker='o', linewidth=3, label='Python')
+    plt.plot(ages_x, js_dev_y, color='#adad3d', linestyle=':', marker='*', linewidth=3, label='JavaScript')
 
     plt.xlabel("Plot Number")
     plt.ylabel("Important var")
     plt.title("Interesting Graph\nCheck it out")
     plt.legend()
+    plt.grid(True)
+    plt.tight_layout()
+    plt.style.use('fivethirtyeight')
 
     plt.show()
+
+
+if __name__ == "__main__":
+    plotLine()
+    plt.xkcd()
+    plotLine()
